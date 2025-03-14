88675d88b8da9f19558fdd7c6dc7fa8359bb64da
Reker1000000 <joshua.j.grayson@gmail.com>
2025-03-14 13:08:53 -0400

    Add files via upload
    Added functionality for selecting songs, pause + resume songs. Will need to download audio files with program so it will play them.

---
+++ Iteration_4_Bumpr.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 14 11:49:40 2025
+
+@author: Josh
+"""
+
+import pygame
+
+pygame.mixer.init()
+
+while True:
+    print("1: Play Back in Black")
+    print("2: Play Thunderstruck")
+    print("3: Play Highway to Hell")
+    print("4: Play You Shook Me All Night Long")
+    print("5: Play Hell's Bells")
+    print("a: Pause song")
+    print("b: Play song")
+    choice = input("Enter your choice")
+    if choice == '1':
+        pygame.mixer.music.load("AC DC - Back In Black (Official 4K Video).mp3")
+        pygame.mixer.music.play()
+    elif choice == '2':
+        pygame.mixer.music.load("AC DC - Thunderstruck (Official Video).mp3")
+        pygame.mixer.music.play()
+    elif choice == '3':
+        pygame.mixer.music.load("AC DC - Highway to Hell (Official Video).mp3")
+        pygame.mixer.music.play()
+    elif choice == '4':
+        pygame.mixer.music.load("AC DC - You Shook Me All Night Long (Official 4K Video).mp3")
+        pygame.mixer.music.play()
+    elif choice == '5':
+        pygame.mixer.music.load("AC DC - Hells Bells (Official 4K Video).mp3")
+        pygame.mixer.music.play()
+    elif choice == 'a':
+        pygame.mixer.music.pause()
+    elif choice == 'b':
+        pygame.mixer.music.unpause()
+
+# Keep the program running while audio plays
+while pygame.mixer.music.get_busy():
+    pass
